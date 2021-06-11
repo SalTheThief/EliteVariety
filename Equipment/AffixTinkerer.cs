8d5e40dec2d517ab87a9e0a32cc5d9446bf9c016
David Galiev <galiev.david@yandex.ru>
2021-06-11 15:13:47 +0500

    Add item displays for Tinkering elites

--- Equipment/AffixTinkerer.cs
+++ Equipment/AffixTinkerer.cs
@@ -30,44 +30,45 @@ namespace EliteVariety.Equipment
             AdjustElitePickupMaterial(new Color32(50, 50, 50, 255), 0.5f, Main.AssetBundle.LoadAsset<Texture>("Assets/EliteVariety/Elites/Tinkerer/texRampEliteTinkerer.png"));
             onSetupIDRS += () =>
             {
-                AddDisplayRule("CommandoBody", "Head", new Vector3(0F, 0.34041F, 0.00382F), new Vector3(0F, 270F, 0F), new Vector3(0.14939F, 0.14939F, 0.14939F));
-                AddDisplayRule("HuntressBody", "Head", new Vector3(0F, 0.29177F, -0.05573F), new Vector3(0F, 270F, 0F), new Vector3(0.09658F, 0.09658F, 0.09658F));
-                AddDisplayRule("Bandit2Body", "Head", new Vector3(-0.00004F, 0.21336F, -0.01352F), new Vector3(346.0624F, 0F, 0F), new Vector3(0.11757F, 0.11757F, 0.11757F));
-                AddDisplayRule("ToolbotBody", "Head", new Vector3(0F, 2.63622F, 2.21049F), new Vector3(358.1617F, 91.44128F, 51.89292F), new Vector3(1.20554F, 1.20554F, 1.20554F));
-                AddDisplayRule("EngiBody", "HeadCenter", new Vector3(0F, 0.17322F, 0F), new Vector3(0F, 270F, 0F), new Vector3(0.13141F, 0.13141F, 0.13141F));
-                AddDisplayRule("EngiTurretBody", "Head", new Vector3(0F, 0.76832F, -0.2789F), new Vector3(0F, 270F, 0F), new Vector3(0.80216F, 0.80385F, 0.80385F));
-                AddDisplayRule("EngiWalkerTurretBody", "Head", new Vector3(0F, 1.3138F, -0.49117F), new Vector3(0F, 270F, 0F), new Vector3(0.79063F, 0.79229F, 0.79229F));
-                AddDisplayRule("MageBody", "Head", new Vector3(-0.00408F, 0.13032F, -0.03472F), new Vector3(0F, 270F, 342.6337F), new Vector3(0.12078F, 0.12078F, 0.12078F));
-                AddDisplayRule("MercBody", "Head", new Vector3(0F, -0.31854F, -0.09837F), new Vector3(0F, 270F, 349.7722F), new Vector3(0.46921F, 0.46921F, 0.46921F));
-                AddDisplayRule("TreebotBody", "FlowerBase", new Vector3(0.07958F, 2.21093F, -0.03811F), new Vector3(0F, 270F, 0F), new Vector3(0.46093F, 0.46093F, 0.46093F));
-                AddDisplayRule("LoaderBody", "Head", new Vector3(0F, 0.24081F, 0.01637F), new Vector3(0F, 270F, 0F), new Vector3(0.14039F, 0.14039F, 0.14039F));
-                AddDisplayRule("CrocoBody", "Head", new Vector3(-0.36997F, 1.26087F, 1.90678F), new Vector3(301.5855F, 249.8843F, 287.3279F), new Vector3(1.48292F, 1.48292F, 1.48292F));
-                AddDisplayRule("CaptainBody", "Head", new Vector3(0.00001F, 0.24554F, 0.04388F), new Vector3(0F, 270F, 0F), new Vector3(0.16429F, 0.16429F, 0.16429F));
+                AddDisplayRule("CommandoBody", "Head", new Vector3(0F, 0.29966F, 0.24059F), new Vector3(345.4218F, 0F, 0F), new Vector3(0.12113F, 0.12113F, 0.12113F));
+                AddDisplayRule("HuntressBody", "Head", new Vector3(0F, 0.26231F, 0.22105F), new Vector3(344.1714F, 0F, 0F), new Vector3(0.09658F, 0.09658F, 0.09658F));
+                AddDisplayRule("Bandit2Body", "Head", new Vector3(-0.00101F, 0.05083F, 0.3401F), new Vector3(5.42616F, 0F, 0F), new Vector3(0.09104F, 0.09104F, 0.09104F));
+                AddDisplayRule("ToolbotBody", "Head", new Vector3(-0.17239F, 4.0752F, -1.83269F), new Vector3(303.0908F, 179.2949F, 1.63074F), new Vector3(1.20554F, 1.20554F, 1.20554F));
+                AddDisplayRule("EngiBody", "HeadCenter", new Vector3(0F, -0.01063F, 0.24091F), new Vector3(11.36212F, 0F, 0F), new Vector3(0.13141F, 0.13141F, 0.13141F));
+                AddDisplayRule("EngiTurretBody", "Head", new Vector3(0F, 1.72741F, 0.27924F), new Vector3(0F, 0F, 0F), new Vector3(0.56685F, 0.56804F, 0.56804F));
+                AddDisplayRule("EngiWalkerTurretBody", "Head", new Vector3(0F, 0.85031F, 1.16546F), new Vector3(0F, 0F, 0F), new Vector3(0.56316F, 0.56434F, 0.56434F));
+                AddDisplayRule("MageBody", "Head", new Vector3(-0.00936F, 0.05186F, 0.18905F), new Vector3(0F, 0F, 0F), new Vector3(0.12078F, 0.12078F, 0.12078F));
+                AddDisplayRule("MercBody", "Head", new Vector3(0F, 0.09905F, 0.22047F), new Vector3(0F, 0F, 0F), new Vector3(0.13241F, 0.13241F, 0.13241F));
+                AddDisplayRule("TreebotBody", "PlatformBase", new Vector3(0F, -0.56082F, 0.90972F), new Vector3(24.55128F, 0F, 0F), new Vector3(0.29737F, 0.29737F, 0.29737F));
+                AddDisplayRule("LoaderBody", "Head", new Vector3(0F, 0.11089F, 0.4385F), new Vector3(0F, 0F, 0F), new Vector3(0.14039F, 0.14039F, 0.14039F));
+                AddDisplayRule("CrocoBody", "Head", new Vector3(-1.97458F, 2.59594F, 0.50771F), new Vector3(339.2264F, 270F, 270F), new Vector3(0.78129F, 0.78129F, 0.78129F));
+                AddDisplayRule("CrocoBody", "Head", new Vector3(2.16853F, 2.59597F, 0.50755F), new Vector3(343.907F, 90F, 90F), new Vector3(0.78129F, 0.78129F, 0.78129F));
+                AddDisplayRule("CaptainBody", "Head", new Vector3(-0.0056F, 0.08758F, 0.28825F), new Vector3(0F, 0F, 0F), new Vector3(0.1122F, 0.1122F, 0.1122F));
                 
-                AddDisplayRule("WispBody", "Head", new Vector3(0F, 0F, 0.70763F), new Vector3(0F, 90F, 90F), new Vector3(0.63005F, 0.63005F, 0.63005F));
-                AddDisplayRule("JellyfishBody", "Hull2", new Vector3(0.06641F, 0.94327F, 0.08761F), new Vector3(359.2905F, 274.3714F, 352.1665F), new Vector3(0.70429F, 0.70429F, 0.70429F));
-                AddDisplayRule("BeetleBody", "Head", new Vector3(0.00703F, 0.43691F, 0.35467F), new Vector3(28.91702F, 319.116F, 341.4145F), new Vector3(0.62749F, 0.62749F, 0.62749F));
+                AddDisplayRule("WispBody", "Head", new Vector3(0F, 0.71386F, -0.0195F), new Vector3(270F, 180F, 0F), new Vector3(0.58181F, 0.58181F, 0.58181F));
+                AddDisplayRule("JellyfishBody", "Hull2", new Vector3(0.30352F, 1.43813F, 0.19542F), new Vector3(282.2123F, 56.21837F, 308.6966F), new Vector3(0.76073F, 0.76073F, 0.76073F));
+                AddDisplayRule("BeetleBody", "Head", new Vector3(0.08712F, 0.48435F, -0.71049F), new Vector3(333.6425F, 178.0887F, 357.6602F), new Vector3(0.43294F, 0.43294F, 0.43294F));
                 AddDisplayRule("LemurianBody", "Head", new Vector3(-0.17158F, 3.78319F, -0.11333F), new Vector3(270F, 0F, 0F), new Vector3(1.43911F, 1.43911F, 1.43911F));
-                AddDisplayRule("HermitCrabBody", "Base", new Vector3(0.00001F, 1.8819F, -0.00001F), new Vector3(0F, 0F, 0F), new Vector3(0.5111F, 0.5111F, 0.5111F));
-                AddDisplayRule("ImpBody", "Neck", new Vector3(0F, 0.07492F, 0.01741F), new Vector3(0F, 90F, 0F), new Vector3(0.24011F, 0.24011F, 0.24011F));
-                AddDisplayRule("VultureBody", "Head", new Vector3(-0.00002F, 1.8647F, -1.27185F), new Vector3(0F, 270F, 82.97913F), new Vector3(1.46533F, 1.21792F, 0.85331F));
-                AddDisplayRule("RoboBallMiniBody", "ROOT", new Vector3(0F, 0.57858F, 0F), new Vector3(0F, 270F, 0F), new Vector3(0.80713F, 0.80713F, 0.80713F));
-                AddDisplayRule("MiniMushroomBody", "Head", new Vector3(-0.63909F, -0.09858F, 0F), new Vector3(0F, 180F, 270F), new Vector3(0.54254F, 0.54254F, 0.54254F));
-                AddDisplayRule("BellBody", "Chain", new Vector3(0F, 0.35436F, 0F), new Vector3(0F, 90F, 180F), new Vector3(1.4342F, 1.4342F, 1.4342F));
-                AddDisplayRule("BeetleGuardBody", "Head", new Vector3(0F, -0.00002F, 2.27553F), new Vector3(0F, 90F, 90F), new Vector3(1.39082F, 1.39082F, 1.39082F));
-                AddDisplayRule("BisonBody", "Head", new Vector3(0.00277F, 0.17604F, 0.79356F), new Vector3(0F, 270F, 270F), new Vector3(0.30597F, 0.30597F, 0.30597F));
-                AddDisplayRule("GolemBody", "Head", new Vector3(0F, 0.80531F, 0.00001F), new Vector3(0F, 270F, 0F), new Vector3(0.68757F, 0.68757F, 0.68757F));
-                AddDisplayRule("ParentBody", "Head", new Vector3(-72.77837F, 127.4748F, -0.57049F), new Vector3(0F, 0F, 42.30991F), new Vector3(50.0322F, 50.0322F, 50.0322F));
-                AddDisplayRule("ClayBruiserBody", "Head", new Vector3(0.00077F, 0.49202F, 0.10396F), new Vector3(0F, 270F, 0.91104F), new Vector3(0.3562F, 0.3562F, 0.3562F));
-                AddDisplayRule("GreaterWispBody", "MaskBase", new Vector3(0F, 0.89092F, 0F), new Vector3(0F, 270F, 0F), new Vector3(0.79403F, 0.79403F, 0.79403F));
-                AddDisplayRule("LemurianBruiserBody", "Head", new Vector3(0F, 2.42635F, 0.04595F), new Vector3(0F, 270F, 270F), new Vector3(1.80072F, 1.80072F, 1.80072F));
-                AddDisplayRule("NullifierBody", "Muzzle", new Vector3(0F, 1.18125F, 0.60903F), new Vector3(12.60184F, 0F, 0F), new Vector3(1.33467F, 1.33467F, 1.33467F));
+                AddDisplayRule("HermitCrabBody", "Base", new Vector3(0.00002F, 1.20259F, 0.74658F), new Vector3(0F, 0F, 0F), new Vector3(0.5111F, 0.5111F, 0.5111F));
+                AddDisplayRule("ImpBody", "Neck", new Vector3(0F, -0.172F, -0.30964F), new Vector3(348.5848F, 180F, 0F), new Vector3(0.13068F, 0.13068F, 0.13068F));
+                AddDisplayRule("VultureBody", "Head", new Vector3(-0.00004F, 5.13124F, -0.62601F), new Vector3(278.3651F, 180F, 180F), new Vector3(1.23141F, 1.23141F, 1.23141F));
+                AddDisplayRule("RoboBallMiniBody", "ROOT", new Vector3(0F, 0.00998F, 1.09398F), new Vector3(0F, 0F, 0F), new Vector3(0.31248F, 0.31248F, 0.28216F));
+                AddDisplayRule("MiniMushroomBody", "Head", new Vector3(0.91357F, -1.02106F, 0F), new Vector3(84.28553F, 90F, 180F), new Vector3(0.26536F, 0.26536F, 0.26536F));
+                AddDisplayRule("BellBody", "Chain", new Vector3(-1.62579F, 1.10939F, -1.08786F), new Vector3(0F, 236.2126F, 180F), new Vector3(0.39593F, 0.39593F, 0.39593F));
+                AddDisplayRule("BeetleGuardBody", "Head", new Vector3(0.17396F, 1.90719F, 0.42468F), new Vector3(276.597F, 348.2153F, 185.774F), new Vector3(0.67094F, 0.67094F, 0.67094F));
+                AddDisplayRule("BisonBody", "Head", new Vector3(-0.00756F, 0.88121F, 0.0533F), new Vector3(270.8393F, 270.0001F, 270F), new Vector3(0.28029F, 0.28029F, 0.28029F));
+                AddDisplayRule("GolemBody", "Head", new Vector3(0F, 0.70964F, 0.73467F), new Vector3(0F, 0F, 0F), new Vector3(0.30644F, 0.30644F, 0.30644F));
+                AddDisplayRule("ParentBody", "Head", new Vector3(62.93F, 100.125F, 0.65524F), new Vector3(317.6959F, 88.84124F, 0.77998F), new Vector3(33.58714F, 33.58714F, 33.58714F));
+                AddDisplayRule("ClayBruiserBody", "Head", new Vector3(0.03907F, 0.40008F, 0.70187F), new Vector3(351.5047F, 3.68439F, 359.9409F), new Vector3(0.20678F, 0.20678F, 0.20678F));
+                AddDisplayRule("GreaterWispBody", "MaskBase", new Vector3(0F, 0.25121F, 1.07853F), new Vector3(0F, 0F, 0F), new Vector3(0.24062F, 0.24062F, 0.24062F));
+                AddDisplayRule("LemurianBruiserBody", "Head", new Vector3(0.00455F, 5.71222F, 0.04572F), new Vector3(270.6796F, 270F, 270F), new Vector3(0.84517F, 0.84517F, 0.84517F));
+                AddDisplayRule("NullifierBody", "Muzzle", new Vector3(0F, -0.42413F, 2.33217F), new Vector3(32.39762F, 0F, 0F), new Vector3(0.44779F, 0.44779F, 0.44779F));
                 
-                AddDisplayRule("BeetleQueen2Body", "Head", new Vector3(0F, 2.58076F, 0.26178F), new Vector3(0F, 90F, 330.7372F), new Vector3(1.46217F, 2.01176F, 2.23879F));
-                AddDisplayRule("ClayBossBody", "PotLidTop", new Vector3(0F, 0.75866F, 1.03199F), new Vector3(0F, 270F, 0F), new Vector3(1.20857F, 1.20857F, 1.20857F));
-                AddDisplayRule("TitanBody", "Head", new Vector3(0F, 6.38225F, 0.23452F), new Vector3(0F, 270F, 0F), new Vector3(2.02218F, 2.02218F, 2.02218F));
-                AddDisplayRule("TitanGoldBody", "Head", new Vector3(0F, 5.42067F, 0.23449F), new Vector3(0F, 270F, 0F), new Vector3(2.02218F, 2.02218F, 2.02218F));
-                AddDisplayRule("VagrantBody", "Hull", new Vector3(0F, 1.62232F, 0F), new Vector3(0F, 270F, 0F), new Vector3(0.98428F, 0.98428F, 0.98428F));
+                AddDisplayRule("BeetleQueen2Body", "Head", new Vector3(-0.07684F, 1.65008F, -2.01268F), new Vector3(0.60901F, 186.126F, 358.306F), new Vector3(0.46099F, 0.46099F, 0.46099F));
+                AddDisplayRule("ClayBossBody", "PotBase", new Vector3(0F, 0.41822F, 3.11682F), new Vector3(0F, 0F, 0F), new Vector3(0.5485F, 0.5485F, 0.5485F));
+                AddDisplayRule("TitanBody", "Head", new Vector3(0.34594F, 1.3132F, 2.81336F), new Vector3(0F, 0F, 0F), new Vector3(0.90732F, 0.90732F, 0.90732F));
+                AddDisplayRule("TitanGoldBody", "Head", new Vector3(0.34594F, 1.3132F, 2.81336F), new Vector3(0F, 0F, 0F), new Vector3(0.90732F, 0.90732F, 0.90732F));
+                AddDisplayRule("VagrantBody", "Hull", new Vector3(0F, 1.30998F, 1.3594F), new Vector3(0F, 0F, 0F), new Vector3(0.38323F, 0.38323F, 0.38323F));
                 string[] worms = new string[]
                 {
                     "MagmaWormBody",
@@ -75,18 +76,14 @@ namespace EliteVariety.Equipment
                 };
                 foreach (string worm in worms)
                 {
-                    for (var i = 1; i <= 16; i++)
-                    {
-                        Vector3 scale = Vector3.one * (1.40164912F - 0.042983125F * (i - 1));
-                        AddDisplayRule(worm, "Neck" + i.ToString(), new Vector3(0F, 0.58245F + 0.08892F * (i - 1), -0.42705F + 0.012033125F * (i - 1)), new Vector3(0F, 90F, 0F), scale);
-                    }
+                    AddDisplayRule(worm, "Head", new Vector3(0F, -1.83452F, -1.28648F), new Vector3(82.25841F, 180.0001F, 0F), new Vector3(0.5628F, 0.5628F, 0.5628F));
                 }
-                AddDisplayRule("RoboBallBossBody", "Shell", new Vector3(0F, 1.21929F, 0F), new Vector3(0F, 270F, 0F), new Vector3(0.57092F, 0.57092F, 0.57092F));
-                AddDisplayRule("SuperRoboBallBossBody", "Shell", new Vector3(0F, 1.21929F, 0F), new Vector3(0F, 270F, 0F), new Vector3(0.57092F, 0.57092F, 0.57092F));
-                AddDisplayRule("GravekeeperBody", "Head", new Vector3(0F, 2.89883F, -0.10267F), new Vector3(0F, 0F, 0F), new Vector3(1.0386F, 1.0386F, 1.0386F));
-                AddDisplayRule("ImpBossBody", "Neck", new Vector3(0F, 0.49847F, -0.04553F), new Vector3(0F, 90F, 358.5645F), new Vector3(1.32086F, 1.32086F, 1.32086F));
-                AddDisplayRule("GrandParentBody", "Head", new Vector3(0F, 1.14019F, -0.55074F), new Vector3(0F, 270F, 0F), new Vector3(1.32372F, 1.32372F, 1.32372F));
-                AddDisplayRule("ScavBody", "Chest", new Vector3(0F, 6.53225F, 2.49379F), new Vector3(0F, 90F, 20.32931F), new Vector3(4.73313F, 4.73313F, 4.73313F));
+                AddDisplayRule("RoboBallBossBody", "MainEyeMuzzle", new Vector3(0F, 0.03414F, -0.05411F), new Vector3(0F, 0F, 0F), new Vector3(0.24644F, 0.24644F, 0.24644F));
+                AddDisplayRule("SuperRoboBallBossBody", "MainEyeMuzzle", new Vector3(0F, 0.03414F, -0.05411F), new Vector3(0F, 0F, 0F), new Vector3(0.24644F, 0.24644F, 0.24644F));
+                AddDisplayRule("GravekeeperBody", "Head", new Vector3(0F, 1.15209F, 3.44118F), new Vector3(342.9781F, 0F, 0F), new Vector3(0.83189F, 0.83189F, 0.83189F));
+                AddDisplayRule("ImpBossBody", "Neck", new Vector3(0.04392F, -1.25395F, -2.24839F), new Vector3(350.0941F, 180.2507F, 358.5428F), new Vector3(0.95173F, 0.95173F, 0.95173F));
+                AddDisplayRule("GrandParentBody", "Head", new Vector3(0F, 1.14006F, 1.14233F), new Vector3(0F, 0F, 0F), new Vector3(0.91952F, 0.91952F, 0.91952F));
+                AddDisplayRule("ScavBody", "Head", new Vector3(0F, 6.55211F, -1.8542F), new Vector3(288.6726F, 180F, 180F), new Vector3(2.64228F, 2.64228F, 2.64228F));
             };
 
             if (Main.aspectAbilitiesEnabled) AspectAbilitiesSupport();
