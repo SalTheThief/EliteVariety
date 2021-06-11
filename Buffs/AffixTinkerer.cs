051f28a75824b929d55e715d18b10fea46732f29
David Galiev <galiev.david@yandex.ru>
2021-06-12 03:29:34 +0500

    Add a spawn range for Tinkerer's Drones

--- Buffs/AffixTinkerer.cs
+++ Buffs/AffixTinkerer.cs
@@ -92,6 +92,8 @@ namespace EliteVariety.Buffs
                         droneSpawner = new DeployableMinionSpawner(body.master, deployableSlot, new Xoroshiro128Plus(Run.instance.seed ^ (ulong)GetInstanceID()))
                         {
                             respawnInterval = 30f,
+                            minSpawnDistance = 10f,
+                            maxSpawnDistance = 25f,
                             spawnCard = MysticsRisky2Utils.BaseAssetTypes.BaseCharacterMaster.characterSpawnCards.Find(x => x.name == "EliteVariety_cscTinkererDrone")
                         };
                         droneSpawner.onMinionSpawnedServer += OnMinionSpawnedServer;
